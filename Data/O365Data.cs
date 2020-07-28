de0f35f710d147b0a21255554f44b32a8586e4a3
Adjusted1 <sweetlawrence205@gmail.com>
2020-07-27 20:51:44 -0700

    razor-form-works-when-supplying-credentials-email-data-is-returned

--- Data/O365Data.cs
+++ Data/O365Data.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Exchange.WebServices.Data;
+﻿using Institutional_Knowledge_Learner_VSTO;
+using Microsoft.Exchange.WebServices.Data;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,47 +14,53 @@ namespace blazor_base
         public string Body { get; set; }
         public int Centroid { get; set; } // eg this msg belongs to which group/centroid?
 
-        public O365Data()
+        //[Required]
+        //[StringLength(1, ErrorMessage = "Password was not entered or failed in transmission to server code")] 
+        public string Username { get; set; }
+        public string Password { get; set; }
+        //public static string GetPass(string Password)
+        //{
+        //    return Password;
+        //}
+        public void GetData() 
         {
             ExchangeService _service;
-
             try
             {
                 //Console.WriteLine("Registering Exchange connection");
-
                 _service = new ExchangeService
                 {
-                    Credentials = new WebCredentials("lsweet4@solano.edu", "-----------------------")
+                    Credentials = new WebCredentials(Username, Password)
                 };
             }
-            catch
+            catch (Exception ex)
             {
                 //Console.WriteLine("new ExchangeService failed. Press enter to exit:");
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
                 return;
             }
-
             // This is the office365 webservice URL
             _service.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
-
             // Prepare seperate class for writing email to the database
             try
             {
-
                 //Console.WriteLine("Reading mail");
-
                 // Read 100 mails
                 foreach (EmailMessage email in _service.FindItems(WellKnownFolderName.Inbox, new ItemView(100)))
                 {
                     Subject = email.Sender.ToString();
                     //Console.WriteLine(email.Sender.ToString());
                 }
-
                 //Console.WriteLine("Exiting");
             }
             catch (Exception e)
             {
-                //Console.WriteLine("An error has occured. \n:" + e.Message);
+                System.Diagnostics.Debug.WriteLine(e.ToString());
             }
+        }
+        public O365Data()
+        {
+            
         }
     }
 }
