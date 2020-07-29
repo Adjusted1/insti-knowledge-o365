25ecd538b0bf0a593fd0af17b61e82097c531e38
Adjusted1 <sweetlawrence205@gmail.com>
2020-07-29 09:50:33 -0700

    checks-if-submit-creds-returned-after-click-before-showing-email-data

--- Data/O365Data.cs
+++ Data/O365Data.cs
@@ -1,6 +1,7 @@
 ﻿using Institutional_Knowledge_Learner_VSTO;
 using Microsoft.Exchange.WebServices.Data;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,25 +10,20 @@ namespace blazor_base
 {
     public class O365Data
     {
-        public int MsgNum { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
-        public int Centroid { get; set; } // eg this msg belongs to which group/centroid?
+        public List<string> Subject = new List<string>();
+        public List<string> Body = new List<string>();
+        public List<string> Centroid = new List<string>();
 
         //[Required]
         //[StringLength(1, ErrorMessage = "Password was not entered or failed in transmission to server code")] 
         public string Username { get; set; }
         public string Password { get; set; }
-        //public static string GetPass(string Password)
-        //{
-        //    return Password;
-        //}
+       
         public void GetData() 
         {
             ExchangeService _service;
             try
             {
-                //Console.WriteLine("Registering Exchange connection");
                 _service = new ExchangeService
                 {
                     Credentials = new WebCredentials(Username, Password)
@@ -35,23 +31,21 @@ namespace blazor_base
             }
             catch (Exception ex)
             {
-                //Console.WriteLine("new ExchangeService failed. Press enter to exit:");
                 System.Diagnostics.Debug.WriteLine(ex.ToString());
                 return;
             }
-            // This is the office365 webservice URL
             _service.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
-            // Prepare seperate class for writing email to the database
             try
             {
-                //Console.WriteLine("Reading mail");
-                // Read 100 mails
-                foreach (EmailMessage email in _service.FindItems(WellKnownFolderName.Inbox, new ItemView(100)))
+                foreach (EmailMessage email in _service.FindItems(WellKnownFolderName.Inbox, new ItemView(1)))
                 {
-                    Subject = email.Sender.ToString();
-                    //Console.WriteLine(email.Sender.ToString());
+                    try
+                    {
+                        Subject.Add(email.Sender.ToString());
+                        Body.Add(email.Body.ToString());
+                    }
+                    catch { }
                 }
-                //Console.WriteLine("Exiting");
             }
             catch (Exception e)
             {
@@ -60,7 +54,6 @@ namespace blazor_base
         }
         public O365Data()
         {
-            
         }
     }
 }
