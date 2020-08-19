fb6f19a35906eac2b0a0448afd3a2d6ee5d1c964
Adjusted1 <sweetlawrence205@gmail.com>
2020-08-18 21:36:31 -0700

    achieved-msg-body-load-without-exception

--- Data/O365Data.cs
+++ Data/O365Data.cs
@@ -14,6 +14,8 @@ namespace blazor_base
         public List<string> Body = new List<string>();
         public List<string> Centroid = new List<string>();
 
+        public bool LoggedIn { get; set; } = false;
+
         //[Required]
         //[StringLength(1, ErrorMessage = "Password was not entered or failed in transmission to server code")] 
         public string Username { get; set; }
@@ -22,27 +24,36 @@ namespace blazor_base
         public void GetData() 
         {
             ExchangeService _service;
+            ItemView view;
             try
             {
                 _service = new ExchangeService
                 {
                     Credentials = new WebCredentials(Username, Password)
                 };
+                LoggedIn = true;
             }
             catch (Exception ex)
             {
+                LoggedIn = false;
                 System.Diagnostics.Debug.WriteLine(ex.ToString());
                 return;
             }
             _service.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
             try
             {
-                foreach (EmailMessage email in _service.FindItems(WellKnownFolderName.Inbox, new ItemView(1)))
+                view  = new ItemView(1);
+                var items = _service.FindItems(WellKnownFolderName.Inbox, view);
+                foreach (var o in items.Items)
                 {
                     try
                     {
-                        Subject.Add(email.Sender.ToString());
-                        Body.Add(email.Body.ToString());
+                        //Subject.Add(email.Sender.ToString());
+                        if (o is EmailMessage)
+                        {
+                            o.Load(new PropertySet(BasePropertySet.FirstClassProperties));
+                            Body.Add(o.Body.Text);
+                        }
                     }
                     catch { }
                 }
